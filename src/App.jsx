f39365d87feb24e6b60656d6f370498f1338f9cf
lien <lien3002@hotmail.com>
2024-02-07 09:43:38 -0500

    instalando bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
-
+import NavBar from "./Components/NavBar.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>titulo</h1>
-    </>
+    <div className="App">
+      <NavBar />
+      {/* <Home />
+      <Services />
+      <HowWeWork />
+      <Benefits />
+      <Contact /> */}
+      <div className="container py-4 px-3 my-4">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <button className="btn btn-primary">Primary button</button>
+      </div>
+      <div className="card" >
+          <div className="card-body">
+          <h5 className="card-title">cartilla</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="https://i.postimg.cc/5247K5Yj/image.jpg" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
 
